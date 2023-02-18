12759d7d6772eb26df8007ea37a496e90ca52d91
Oğuzhan Kalkuz <kalkuzoguzhann@gmail.com>
2023-02-18 11:33:13 +0300

    Unit tests for weighted sampler

---
+++ Tests/Runtime/Sampling/Weighted Sampling/WeightedSamplerTest.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Assertions;
+using UnityEngine.TestTools;
+
+namespace KalkuzSystems.Utility.Tests
+{
+  public class WeightedSamplerTests
+  {
+    [UnityTest]
+    public IEnumerator TestSample()
+    {
+      // Define a list of weighted items
+      var items = new List<WeightedItem<string>>
+      {
+        new("apple", 1),
+        new("banana", 2),
+        new("cherry", 3)
+      };
+
+      // Initialize a weighted sampler
+      var sampler = new WeightedSampler<string>(items);
+
+      // Generate 10000 samples and count the number of times each item is selected
+      var counts = new Dictionary<string, int>();
+      for (int i = 0; i < 10000; i++)
+      {
+        var sample = sampler.Sample();
+        if (!counts.ContainsKey(sample))
+        {
+          counts[sample] = 0;
+        }
+
+        counts[sample]++;
+      }
+
+      yield return null;
+
+      // Check that the counts are proportional to the weights
+      Assert.AreApproximatelyEqual(counts["apple"], 10000f / 6, 100f);
+      Assert.AreApproximatelyEqual(counts["banana"], 20000f / 6, 100f);
+      Assert.AreApproximatelyEqual(counts["cherry"], 30000f / 6, 100f);
+    }
+  }
+}

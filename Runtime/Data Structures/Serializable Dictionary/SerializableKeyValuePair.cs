6e2693fad08025d59f24985bc9643b90bb6e9a75
Oğuzhan Kalkuz <kalkuzoguzhann@gmail.com>
2023-03-26 12:33:15 +0300

    Support for serializable dictionaries. Closes #2

---
+++ Runtime/Data Structures/Serializable Dictionary/SerializableKeyValuePair.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace KalkuzSystems.Utility
+{
+  [System.Serializable]
+  public sealed class SerializableKeyValuePair<TKey, TValue>
+  {
+    [SerializeField] private TKey key;
+    [SerializeField] private TValue value;
+    
+    public TKey Key => key;
+    public TValue Value => value;
+
+    public SerializableKeyValuePair(TKey key, TValue value)
+    {
+      this.key = key;
+      this.value = value;
+    }
+  }
+}
